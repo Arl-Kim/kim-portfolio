62af3c4396f8dbcd752364fa89f696bed64d5490
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-03-28 06:09:27 +0300

    Toggle active class for navigation links

--- script.js
+++ script.js
@@ -14,7 +14,25 @@ themeToggler.addEventListener("click", () => {
 
 //Scroll Sections
 
+let sections = document.querySelectorAll("section");
+let navigationLinks = document.querySelectorAll("header nav a");
+
 window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if(top >= offset && top < offset + height){
+            //Active Nav Links
+            navigationLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
     //Sticky Header
     let header = document.querySelector("header");
 
