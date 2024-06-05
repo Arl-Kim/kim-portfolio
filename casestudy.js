1cb37fe7cf6162aea9383634adfa1d618aa3b3e5
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-06-05 16:52:57 +0300

    Define projects data in an array and define functions to populate the data

---
+++ casestudy.js
@@ -0,0 +1,268 @@
+const projectData = [
+    {
+      id: 'moocourses',
+      title: 'Transfrontières MooCourses Project',
+      industry: 'Education',
+      shortDescription: 'Transfrontières\' online learning platform with customised EdTech tools through which learners and tutors interact and share knowledge.',
+      image: '/assets/moocourses_proj_long.gif',
+      duration: '3 Months',
+      launched: 'April 2024',
+      projectOverview: 'The Transfrontières MooCourses Project aimed to revolutionize online learning by providing a robust platform with customized EdTech tools. It facilitates interaction and knowledge sharing among learners and tutors, making the Education and Training service at Transfrontières Agencies Limited more accessible and effective. Through the platform, clients can now learn French Language for Professional Purposes, ICT, Music, and Research. I built a highly scalable system to allow for additional education and training modules which are due to be added over the next few months. MooCourses has been highly rated by users for its comprehensive approach to online learning and its innovative features.',
+      features: [
+        'Custom EdTech Tools',
+        'Interactive Learning Modules',
+        'Real-Time Collaboration',
+        'Highly Scalable',
+        'Secure User Authentication'
+      ],
+      challenges: [
+        {
+          heading: 'Technical Constraints',
+          description: 'Implementing real-time collaboration features with low latency was a significant technical challenge that required careful optimization.'
+        },
+        {
+          heading: 'User Engagement',
+          description: 'Ensuring consistent user engagement in a virtual learning environment was difficult, necessitating the development of interactive and gamified elements.'
+        }
+      ],
+      solutions: [
+        {
+          heading: 'Optimization Techniques',
+          description: 'I used advanced optimization techniques and robust server architecture to minimize latency and ensure seamless real-time collaboration.'
+        },
+        {
+          heading: 'Interactive & Gamified Elements',
+          description: 'I integrated interactive and gamified elements such as quizzes, badges, and leaderboards to enhance user engagement and make learning more enjoyable.'
+        }
+      ],
+      impact: [
+        'The MooCourses platform has significantly improved the accessibility of education and training for individual students and businesses across Kenya and other parts of the world, allowing them to learn from anywhere. The real-time collaboration features have enabled more effective tutor-student interactions, thereby enhancing the overall learning experience. Including highly interactive modules in the platform has significantly increased user engagement, with more students completing their courses. Through this project Transfrontieres Agencies Limited experienced a massive transformation in how they offer their Education and Training service to their clients. It perfectly aligned with their objective of full service digitalization.'
+      ],
+      techStackDescription: 'For this project, I utilized a robust technology stack. HTML5 and CSS3 were used for front-end development to create a responsive and user-friendly interface. PHP was employed for back-end development, handling server-side logic and operations. MySQL was chosen as the database management system to efficiently store and manage user and course data.',
+      techStack: [
+        { name: 'HTML5', logo: '/assets/html.svg' },
+        { name: 'CSS3', logo: '/assets/css.svg' },
+        { name: 'PHP', logo: '/assets/php.svg' },
+        { name: 'MySQL', logo: '/assets/mysql_server.svg' }
+      ]
+    },
+    {
+        id: 'raasis',
+        title: 'Ra\'Asis Web App: Connecting Clients with Services',
+        industry: 'Software Development',
+        shortDescription: 'Developing a dynamic React web application to link Ra\'Asis clients directly with services and development teams.',
+        image: '/assets/raasis_proj_long.gif',
+        duration: '3 Months',
+        launched: 'May 2024',
+        projectOverview: 'Ra\'Asis Web App is a dynamic platform designed to bridge the gap between Ra\'Asis clients and their services and development teams. Built with React, this web application offers an intuitive and efficient way for clients to access services, track progress, and interact with development teams. The application ensures a seamless user experience, facilitating effective communication and project management.',
+        features: [
+          'User-friendly interface',
+          'Real-time updates',
+          'Direct communication with development teams',
+          'Service tracking and management'
+        ],
+        challenges: [
+          {
+            heading: 'Scalability',
+            description: 'Ensuring the web application can handle increasing numbers of users and data without compromising performance.'
+          },
+          {
+            heading: 'User Engagement',
+            description: 'Creating an engaging user experience that encourages clients to actively use the platform and communicate with development teams.'
+          }
+        ],
+        solutions: [
+          {
+            heading: 'Optimized Architecture',
+            description: 'Implemented a scalable architecture using Node.js and React, allowing the application to grow with the user base.'
+          },
+          {
+            heading: 'Interactive Features',
+            description: 'Developed interactive features and real-time updates to keep users engaged and informed throughout their project lifecycle.'
+          }
+        ],
+        impact: [
+          'Enhanced client satisfaction through direct and efficient communication with development teams, leading to better project outcomes.',
+          'Improved project management and tracking capabilities, allowing clients to monitor progress and make informed decisions in real-time.',
+          'Scalable solution that supports future growth, ensuring the platform remains robust and reliable as the user base expands.'
+        ],
+        techStackDescription: 'The Ra\'Asis Web App leverages a modern technology stack to deliver a seamless and efficient user experience. Utilizing React for the frontend and Node.js for the backend, the platform is designed to be highly responsive and scalable, meeting the demands of a growing user base.',
+        techStack: [
+          { name: 'React', logo: '/assets/react.svg' },
+          { name: 'Node.js', logo: '/assets/nodejs.svg' },
+          { name: 'JavaScript', logo: '/assets/javascript.svg' },
+          { name: 'CSS', logo: '/assets/css.svg' }
+        ]
+      },
+      {
+        id: 'oaksafrica',
+        title: 'The Oaks Africa Foundation Project',
+        industry: 'Non-Profit',
+        shortDescription: 'An online platform for Oaks Africa Foundation to provide information about their mission, vision and community development projects and initiatives. It facilitates a seamless interface to run OAF scholarship and sponsorship programs online.',
+        image: '/assets/oaksafrica_proj_long.gif',
+        duration: '2 Months',
+        launched: 'August 2023',
+        projectOverview: 'The Oaks Africa Foundation website project aimed to create a visually appealing, user-friendly, and responsive platform that aligned with the foundation\'s objectives. Through the platform, interested parties can get information about the foundation\'s agenda, mission, vision scholarship and sponsorship programs and many other OAF projects. It offers interactive ways for visitors to apply for scholarships, donate funds, apply for sponsorship programs, and start sponsoring. The project also included the development of a content management system (CMS) for easy content updates and maintenance, along with integrating basic and advanced SEO practices to enhance visibility in search engine results.',
+        features: [
+          'Responsive Design',
+          'CMS For Easy Content Updates',
+          'Dynamic Scholarship Application Forms',
+          'Dynamic Sponsorship Application Forms',
+          'Secure Donation & Sponsorship Payment Integrations',
+          'Newsletter Subscription & Auto-Updates',
+        ],
+        challenges: [
+          {
+            heading: 'Content Management',
+            description: 'Developing an intuitive CMS that allowed the foundation\'s team to easily update and manage content without deep technical expertise was one of the challenges encountered.'
+          },
+          {
+            heading: 'User Engagement',
+            description: 'Ensuring high user engagement and interaction through the website, particularly for applying to programs and donating to fund the programs.'
+          }
+        ],
+        solutions: [
+          {
+            heading: 'User-Friendly CMS',
+            description: 'I implemented a WordPress-based CMS, utilizing customized theme templates and features that provided easy content management by the foundation\'s team.'
+          },
+          {
+            heading: 'Interactive Features',
+            description: 'The team also developed interactive features such as application forms and donation integrations to enhance user engagement and interaction.'
+          }
+        ],
+        impact: [
+          'The website has significantly increased the foundation’s online visibility, leading to higher traffic and increased engagement with the community. The interactive scholarship and sponsorship applications, secure online donation payments and constant communication channels through newsletters and website adverts have simplified the process for users and elevated their experiences. The results are evident in more scholarship applications and donations as compared to when the process was only handled offline. The user-friendly CMS has empowered the foundation’s team to manage and update content efficiently without needing frequent technical support.'
+        ],
+        techStackDescription: 'For this OAF project, I utilized a robust technology stack to ensure a responsive, user-friendly, and highly functional website. The front-end was built using HTML, CSS, and JavaScript to create a dynamic and engaging user experience. The back-end was powered by WordPress, PHP, and MySQL, providing a robust and scalable platform for content management and data handling. IntaSend API was integrated for secure and efficient payment processing in online donations and sponsorship funding.',
+        techStack: [
+          { name: 'WordPress', logo: '/assets/wordpress.svg' },
+          { name: 'HTML', logo: '/assets/html.svg' },
+          { name: 'CSS', logo: '/assets/css.svg' },
+          { name: 'JavaScript', logo: '/assets/javascript.svg' },
+          { name: 'PHP', logo: '/assets/php.svg' },
+          { name: 'MySQL', logo: '/assets/mysql_server.svg' }
+        ]
+      },
+      {
+        id: 'transfrontieres',
+        title: 'Transfrontières Agencies Website',
+        industry: 'Multi-Service',
+        shortDescription: 'A comprehensive website for Transfrontières Agencies Limited to connect clients with a wide range of integrated services, including Communications, Education & Training, ICT, and Events.',
+        image: '/assets/transfro_proj_long.gif',
+        duration: '2 Months',
+        launched: 'July 2023',
+        projectOverview: 'The Transfrontières Agencies Website is an Integrated Services platform that was developed to streamline the connection between the agency and its clients, primarily businesses and brands. The website offers an easy-to-navigate interface where clients can book consultations and directly access digital solutions in Communications, Education & Training, ICT, and Events through secure links.',
+        features: [
+          'User-friendly Interface',
+          'Online Booking System For Consultations',
+          'Secure Access To Digital Solutions',
+          'Service Categories: Communications, Education & Training, ICT, Events',
+          'Responsive Design For All Screen Sizes'
+        ],
+        challenges: [
+          {
+            heading: 'Service Integration',
+            description: 'Integrating all the services including their sub-services into a single cohesive platform required a robust and flexible architecture.',
+          },
+          {
+            heading: 'User Experience',
+            description: 'Ensuring a seamless user experience across diverse services and client needs was a complex task, necessitating meticulous design and testing.'
+          },
+          {
+            heading: 'Security',
+            description: 'Implementing secure access to digital solutions and ensuring data protection across all services was also one of the main challenges in this project.'
+          }
+        ],
+        solutions: [
+          {
+            heading: 'Modular Architecture',
+            description: 'I developed a modular architecture that allows for flexible integration of various services, making it easy to manage and scale the platform.'
+          },
+          {
+            heading: 'Enhanced UX Design',
+            description: 'Through user research and iterative design, I created an intuitive and seamless user experience that caters to individuals of all ages and businesses in different levels.'
+          },
+          {
+            heading: 'Advanced Security Measures',
+            description: 'I implemented secure links and robust authentication mechanisms to protect user data and ensure safe access to all the available digital solutions.'
+          }
+        ],
+        impact: [
+          'The integrated services on the website have significantly improved client engagement, providing a centralized platform for accessing all the offerings of Transfrontières Agencies Ltd. The online booking system has streamlined all consultation processes, making it more efficient and convenient for Transfrontières\' clients. The secure linking to the available digital solutions per service have increased client trust and satisfaction, leading to positive feedback and higher retention rates.'
+        ],
+        techStackDescription: 'I used WordPress for a flexible CMS, with HTML, CSS, and JavaScript for front-end development. PHP was utilized for back-end processes, and MySQL for database management, ensuring a robust, scalable, and secure platform.',
+        techStack: [
+          { name: 'WordPress', logo: '/assets/wordpress.svg' },
+          { name: 'HTML5', logo: '/assets/html.svg'},
+          {name: 'CSS3', logo: '/assets/css.svg'},
+          {name: 'JavaScript', logo: '/assets/javascript.svg'},
+          {name: 'PHP', logo: '/assets/php.svg'},
+          {name: 'MySQL', logo: '/assets/mysql_server.svg'}
+        ]
+      }
+  ];
+  
+  function populateCaseStudyPage(projectId) {
+    const project = projectData.find(item => item.id === projectId);
+    if (project) {
+      document.getElementById('case_study_industry').textContent = project.industry;
+      document.getElementById('case_study_title').textContent = project.title;
+      document.getElementById('case_study_short_description').textContent = project.shortDescription;
+      document.getElementById('case_study_image').src = project.image;
+      
+      document.getElementById('case_study_industry').textContent = project.industry;
+      document.getElementById('case_study_duration').textContent = project.duration;
+      document.getElementById('case_study_launched').textContent = project.launched;
+  
+      document.getElementById('project_overview').textContent = project.projectOverview;
+  
+      const featuresList = document.getElementById('key_features');
+      featuresList.innerHTML = '';
+      project.features.forEach(feature => {
+        const listItem = document.createElement('li');
+        listItem.textContent = feature;
+        featuresList.appendChild(listItem);
+      });
+  
+      const challengesSection = document.getElementById('challenges_section');
+      challengesSection.innerHTML = '';
+      project.challenges.forEach(challenge => {
+        const challengeDiv = document.createElement('div');
+        challengeDiv.innerHTML = `<h3>${challenge.heading}</h3><p>${challenge.description}</p>`;
+        challengesSection.appendChild(challengeDiv);
+      });
+  
+      const solutionsSection = document.getElementById('solutions_section');
+      solutionsSection.innerHTML = '';
+      project.solutions.forEach(solution => {
+        const solutionDiv = document.createElement('div');
+        solutionDiv.innerHTML = `<h3>${solution.heading}</h3><p>${solution.description}</p>`;
+        solutionsSection.appendChild(solutionDiv);
+      });
+  
+      const impactSection = document.getElementById('impact_section');
+      impactSection.innerHTML = `<p>${project.impact.join('</p><p>')}</p>`;
+  
+      document.getElementById('tech_stack_description').textContent = project.techStackDescription;
+      const techStackCards = document.getElementById('tech_stack_section');
+      techStackCards.innerHTML = '';
+      project.techStack.forEach(tech => {
+        const techCard = document.createElement('div');
+        techCard.classList.add('technology');
+        techCard.innerHTML = `<div class="tech-image"><img src="${tech.logo}" alt="${tech.name}"></div><div class="tech-text"><p>${tech.name}</p></div>`;
+        techStackCards.appendChild(techCard);
+      });
+    } else {
+      console.error('Project not found!');
+    }
+  }
+  
+  function handleCaseStudyPage() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const projectId = urlParams.get('id');
+    populateCaseStudyPage(projectId);
+  }
+  
+  document.addEventListener('DOMContentLoaded', handleCaseStudyPage);
+  
